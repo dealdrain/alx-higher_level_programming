0c965b09599b66a607605a8a1e2fd257a88de2c2
dealdrain <dealdrain@yahoo.com>
2023-10-05 12:00:09 -0700

    2. How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
 total = len(sys.argv)
 if total <= 1:
-    print("0 argument.")
+    print("0 arguments.")
 else:
     print(f"{total - 1} {'argument' if total == 2 else 'arguments'}:")
     for i, arg in enumerate(sys.argv[1:], 1):
