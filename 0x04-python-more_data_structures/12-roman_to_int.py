b03ee393eb7502669bb654724d01d56cedb825b2
dealdrain <dealdrain@yahoo.com>
2023-10-11 13:52:12 -0700

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     roman_numerals = {
             'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1
-            }
+    }
 
     num = 0
     last = 0
