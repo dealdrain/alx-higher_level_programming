f5ca19e64b62c7faea706152cdebfbe5f58d9981
dealdrain <dealdrain@yahoo.com>
2023-10-23 10:09:19 -0700

    5. Raise exception

--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -1,6 +1,7 @@
-se_exception = __import__('5-raise_exception').raise_exception
+#!/usr/bin/python3
+raise_exception = __import__('5-raise_exception').raise_exception
 
 try:
-    raise_exception()
+        raise_exception()
 except TypeError as te:
-    print("Exception raised")
+        print("Exception raised")
