63e4f572017043db0ffc876467963ea7e909fe47
dealdrain <dealdrain@yahoo.com>
2023-10-23 09:43:51 -0700

    3. Integers division with debug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print('{:d}'.format(my_list[i]), end='')
-            count += 1
-        except (ValueError, TypeError):
-            pass
-        print()
+    try:
+        count = 0
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                count += 1
+                print("{:d}".format(my_list[i]), end="")
+    except TypeError as err:
+        print(err)
+    else:
+        print("")
         return count
