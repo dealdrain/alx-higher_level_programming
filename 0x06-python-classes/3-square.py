6a201baf3dbd142dc7ab0aea340f5b00faadbd54
dealdrain <dealdrain@yahoo.com>
2023-10-24 11:11:20 -0700

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python3
+"""A Square class that will be used to instantiate an object"""
 
 
 class Square:
-    """Square class."""
+
+    """An ampty class Square that defines a square.
+    It currently has no attributes nor properties.
+    """
 
     def __init__(self, size=0):
-        """__init__ method that sets the size of square.
-        Args:
-            size (int): size of Square
-        Raises:
-            TypeError: If `size` is not an integer.
-            ValueError: If `size` is less than 0.
+        """Initialization method of the class
+                Args:
+                        size (int): Size of the square
         """
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
-        """Gets the area of the Square.
-
-        Returns:
-            Area of squre
-        """
-        return self.__size * self.__size
-
-
-if __name__ == '__main__':
-    my_square_1 = Square(3)
-    print("Area: {}".format(my_square_1.area()))
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    my_square_2 = Square(5)
-    print("Area: {}".format(my_square_2.area()))
+        """Calculate the area of the square instance
+                Returns:
+                        Integer: Square of the size
+                """
+        return (self.__size ** 2)
