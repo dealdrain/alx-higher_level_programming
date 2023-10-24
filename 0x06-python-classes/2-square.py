c99390817eb6f9c2314e4bd15df79178230bc8f1
dealdrain <dealdrain@yahoo.com>
2023-10-24 10:21:36 -0700

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ This is a "Square"  module.
 
 class Square:
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
