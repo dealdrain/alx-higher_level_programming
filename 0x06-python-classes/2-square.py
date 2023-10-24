e43eb5b1f2e136af0507753f4e7e76253a7b20a6
dealdrain <dealdrain@yahoo.com>
2023-10-24 10:24:18 -0700

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,6 +6,7 @@ This is a "Square"  module.
 
 
 class Square:
+    """A class that defines a square by size."""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
