46d5c210c76920b53f20eafecf698a245d9c5c57
dealdrain <dealdrain@yahoo.com>
2023-10-24 10:07:01 -0700

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
+"""
+This is a "Square"  module.
+"""
+
 
 class Square:
+    """A class that defines a Square by size"""
     def __init__(self, size):
         self.__size = size
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
-
