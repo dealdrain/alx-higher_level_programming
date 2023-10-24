9acf411ebc51055c290dc1c7afb08210711dbbf6
dealdrain <dealdrain@yahoo.com>
2023-10-24 10:57:55 -0700

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-This is the "Square"  module.
-
-This module provides a simple Square class with initialize size.
-Defaults size to 0. Raise error on invalid size inputs.
-Method area returns size of area of the square.
-
+This is a "Square"  module.
 """
 
 
 class Square:
-    """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
@@ -20,3 +14,21 @@ class Square:
 
     def area(self):
         return self.__size * self.__size
+
+
+if __name__ == '__main__':
+    my_square_1 = Square(3)
+    print("Area:", my_square_1.area())
+
+    try:
+        print(my_square_1.size)
+    except AttributeError as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except AttributeError as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area:", my_square_2.area())
