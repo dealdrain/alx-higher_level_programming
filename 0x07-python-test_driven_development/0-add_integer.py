8579026167021f0c6e9f58c08e1acb80da7addcb
dealdrain <dealdrain@yahoo.com>
2023-10-31 15:55:55 -0700

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """
-"Add Integer"  module.
+This is the "Add Integer"  module.
 
+This module supplies one function, add_integer(),
+which adds together 2 int or float types and returns an int.
 """
 
 
 def add_integer(a, b):
-    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+    """Return the sum of two integers or floats as an integer.
+    Otherwise raise a TypeError for given incorrect argument type.
+    """
+    h = list(map(lambda x: isinstance(x, (int, float)), [a, b]))
+
+    if all(h):
         return int(a) + int(b)
-    else:
-        raise TypeError("Both arguments must be integers or floats")
+
+    for x, y in list(zip(h, ['a', 'b'])):
+        if not x:
+            raise TypeError("{} must be an integer".format(y))
