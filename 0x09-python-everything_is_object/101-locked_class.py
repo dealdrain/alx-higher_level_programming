f879fde3d5f37a03d0c976c03ba67711740f571d
dealdrain <dealdrain@yahoo.com>
2023-10-31 15:29:36 -0700

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,15 +2,10 @@
 
 
 class LockedClass:
-    """Locked Class
+    """Locked Clas
     """
     __slots__ = ['first_name']
 
-
-if __name__ == '__main__':
-    lc = LockedClass()
-    lc.first_name = "John"
-    try:
-        lc.last_name = "Snow"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    def __init__(self):
+        """Init class method nothing much"""
+        pass
