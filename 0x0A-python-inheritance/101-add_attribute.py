56dcee61dbd10998a2087aece26c3ddc77c9808e
dealdrain <dealdrain@yahoo.com>
2023-11-06 09:19:37 -0800

    11

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define class MyInt int."""
+
+
+class MyInt(int):
+    """num int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
