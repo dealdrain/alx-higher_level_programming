1ad5322d58f037ff4381259455f3ddad1fc7f4ad
dealdrain <dealdrain@yahoo.com>
2023-11-06 06:06:55 -0800

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 
+"""Function that looks up the methods of an object"""
+
 
 def lookup(obj):
-    """list of available attributes
+    """Checks what methods are available in an object
+    Arguments: obj - Object to be checked
+    Return: A list
     """
     return dir(obj)
-
-
-if __name__ == '__main__':
-    class MyClass1(object):
-        pass
-
-    class MyClass2(object):
-        my_attr1 = 3
-
-        def my_meth(self):
-            pass
-
-    print(lookup(MyClass1))
-    print(lookup(MyClass2))
-    print(lookup(int))
