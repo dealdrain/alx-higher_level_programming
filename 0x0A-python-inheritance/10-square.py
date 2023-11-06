0753655f1881720cd06338b485f53a2c344c3f09
dealdrain <dealdrain@yahoo.com>
2023-11-06 08:38:10 -0800

    10. Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Define Rec subclass sq."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Rep a sq."""
+
     def __init__(self, size):
+        """Initialization.
+        Args:
+            size (int): new square size.
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
