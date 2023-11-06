72fa1adcc459f6fb682a7a08051cb0a529ee0b69
dealdrain <dealdrain@yahoo.com>
2023-11-06 07:46:58 -0800

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Rep base geo."""
+
+    def area(self):
+        """Not deployed."""
+        raise Exception("area() is not implemented")
