57996e9993fea7e19f31d4174d5d9e21e3dca042
dealdrain <dealdrain@yahoo.com>
2023-11-06 06:37:10 -0800

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-"""A class definition for MyList"""
+"""A class def for MyList"""
 
 
 class MyList(list):
 
-    """MyList inherits from the list class"""
-
     def print_sorted(self):
-        """Sorts the elements in the class"""
         list_sorted = sorted(self)
         print(list_sorted)
 
