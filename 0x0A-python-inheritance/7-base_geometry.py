5e93de174fd991d49a6b3be03792ae678af9e3e2
dealdrain <dealdrain@yahoo.com>
2023-11-06 08:45:26 -0800

    7. Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Defines a base geo class BaseGeometry."""
+"""Defines a base geo."""
 
 
 class BaseGeometry:
-    """Rep base."""
+    """Rep base geo."""
 
     def area(self):
+        """Not deployed."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a par wiht int.
+        """Validate a parameter as an integer.
 
         Args:
-            name (str): Par name.
-            value (int): Par lookupe.
+            name (str):name of par.
+            value (int): lookup par
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+            TypeError: value not int.
+            ValueError: value <= 0.
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
