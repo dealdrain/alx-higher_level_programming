7ba9ca4bec30e6dda6ff10e46fe7c28420ce5e08
dealdrain <dealdrain@yahoo.com>
2023-11-07 20:48:10 -0800

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""Defines a text file with line-counting function."""
+
+
 def read_lines(filename="", nb_lines=0):
-    with open(filename, encoding="utf-8") as fd:
-        if (nb_lines == 0):
-            print(fd.read(), end="")
-        else:
-            for line in fd:
-                if (nb_lines > 0):
-                    print(line, end="")
-                    nb_lines -= 1
+    """Returns the number of lines in a text file."""
+
+
+with open(filename, encoding="utf-8") as fd:
+    if (nb_lines == 0):
+        print(fd.read(), end="")
+    else:
+        for line in fd:
+            if (nb_lines > 0):
+                print(line, end="")
+                nb_lines -= 1
