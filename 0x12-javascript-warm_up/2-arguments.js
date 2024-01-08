d403a35cd3855a8425a22962e30ad3c844e90adf
dealdrain <dealdrain@yahoo.com>
2024-01-08 13:14:55 -0800

    tasks 0 to 5

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let x;
+if (process.argv.length < 3) {
+  x = 'No argument';
+} else if (process.argv.length === 3) {
+  x = 'Argument found';
+} else {
+  x = 'Arguments found';
+}
+console.log(x);
