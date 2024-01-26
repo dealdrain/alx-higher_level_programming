134b9369bf33d7357b32782f47a085c807de9a1e
dealdrain <dealdrain@yahoo.com>
2024-01-26 02:11:35 -0800

    4. Whats my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+from requests import get
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    response = get(url)
+    bytes_content = response.text
+    string = 'Body response:\n\t- type: {}\n\t- content: {}'.format(
+             type(bytes_content), bytes_content)
+    print(string)
