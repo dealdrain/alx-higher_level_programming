f93b67cdc2d5f353df3322680c09b815b1da4904
dealdrain <dealdrain@yahoo.com>
2024-01-26 08:36:39 -0800

    correction task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ fetches https://alx-intranet.hbtn.io/status
 
 from urllib import request
 
-with request.urlopen("https://intranet.hbtn.io/status") as response:
+with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     r = response.read()
     print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
           .format(type(r), r, r.decode('utf-8')))
