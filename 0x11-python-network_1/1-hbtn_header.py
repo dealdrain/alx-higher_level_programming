c34ef58cfb92f5904afe167788931acc2df30c3e
dealdrain <dealdrain@yahoo.com>
2024-01-26 08:23:17 -0800

    correction task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
+takes in a URL, sends a request to the URL and displays the value of
+the X-Request-Id
 """
 
 
